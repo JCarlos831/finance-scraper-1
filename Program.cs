323e5009a580a5c06e99cfd46ec273b8e6a7bf95
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-06-26 16:21:07 -0400

    Updated project to netcoreapp2.1 and added table headers to console

--- Program.cs
+++ Program.cs
@@ -17,7 +17,7 @@ namespace finance_scraper_1
     {
         public static void Main(string[] args)
         {
-            FirefoxDriverService service = FirefoxDriverService.CreateDefaultService("/Users/JuanCMontoya/Projects/vscode/csharp/finance-scraper-0/bin/Debug/netcoreapp2.0");
+            FirefoxDriverService service = FirefoxDriverService.CreateDefaultService("/Users/JuanCMontoya/Projects/vscode/csharp/finance-scraper-1/bin/Debug/netcoreapp2.1");
             var driver = new FirefoxDriver(service);
 
             // Open the browser and navigate to the Yahoo! Finance login page.
@@ -27,13 +27,13 @@ namespace finance_scraper_1
             driver.Manage().Window.Maximize();
             
             // If element cannot be found in ten seconds, timeout.
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
 
             // Find username text box, enter username, and press Enter.
             driver.FindElement(By.Id("login-username")).SendKeys("testfinance@yahoo.com" + Keys.Enter);
 
             // If element cannot be found in ten seconds, timeout.
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
 
             // Find password text box, enter password, and press Enter.
             driver.FindElement(By.Id("login-passwd")).SendKeys("3eggWhites6Almonds" + Keys.Enter);
@@ -46,7 +46,7 @@ namespace finance_scraper_1
             driver.FindElement(By.XPath("/html/body/div[2]/div[3]/section/section/div[2]/table/tbody/tr[1]/td[1]/a")).Click();
 
             // If element cannot be found in ten seconds, timeout.
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
 
             // Find table with stock data
             IWebElement table = driver.FindElement(By.ClassName("_1TagL"));
@@ -54,6 +54,37 @@ namespace finance_scraper_1
             // Find all rows in the table
             IList<IWebElement> rows = new List<IWebElement>(table.FindElements(By.TagName("tr")));
             String strRowData = "";
+
+            // Get Table Headers
+            String strThData = "";
+            // get table header for stock symbols
+            strThData = strThData + driver.FindElement(By.XPath("/html/body/div[2]/div[3]/section/section[2]/div[1]/table/thead/tr/th[1]/span")).Text + "\t";
+            // get table header for last price
+            strThData = strThData + driver.FindElement(By.XPath("/html/body/div[2]/div[3]/section/section[2]/div[1]/table/thead/tr/th[2]")).Text + "\t";
+            // get table header for change
+            strThData = strThData + driver.FindElement(By.XPath("/html/body/div[2]/div[3]/section/section[2]/div[1]/table/thead/tr/th[3]")).Text + "\t";
+            // get table header for %chg
+            strThData = strThData + driver.FindElement(By.XPath("/html/body/div[2]/div[3]/section/section[2]/div[1]/table/thead/tr/th[4]")).Text + "\t";
+            // get table header for currency
+            strThData = strThData + driver.FindElement(By.XPath("/html/body/div[2]/div[3]/section/section[2]/div[1]/table/thead/tr/th[5]")).Text + "\t";
+            // get table header for market time
+            strThData = strThData + driver.FindElement(By.XPath("/html/body/div[2]/div[3]/section/section[2]/div[1]/table/thead/tr/th[6]")).Text + "\t";
+            // get table header for volume
+            strThData = strThData + driver.FindElement(By.XPath("/html/body/div[2]/div[3]/section/section[2]/div[1]/table/thead/tr/th[7]")).Text + "\t";
+            // get table header for shares
+            strThData = strThData + driver.FindElement(By.XPath("/html/body/div[2]/div[3]/section/section[2]/div[1]/table/thead/tr/th[8]")).Text + "\t";
+            // get table header for avg volume (3m)
+            strThData = strThData + driver.FindElement(By.XPath("/html/body/div[2]/div[3]/section/section[2]/div[1]/table/thead/tr/th[9]")).Text + "\t";
+            // get table header for day range
+            strThData = strThData + driver.FindElement(By.XPath("/html/body/div[2]/div[3]/section/section[2]/div[1]/table/thead/tr/th[10]")).Text + "\t";
+            // get table header for 52-wk range
+            strThData = strThData + driver.FindElement(By.XPath("/html/body/div[2]/div[3]/section/section[2]/div[1]/table/thead/tr/th[11]")).Text + "\t";
+            // get table header for day chart
+            strThData = strThData + driver.FindElement(By.XPath("/html/body/div[2]/div[3]/section/section[2]/div[1]/table/thead/tr/th[12]")).Text + "\t";
+            // get table header for market cap
+            strThData = strThData + driver.FindElement(By.XPath("/html/body/div[2]/div[3]/section/section[2]/div[1]/table/thead/tr/th[13]")).Text;
+            // print table headers to console
+            System.Console.WriteLine(strThData);
 
             // Go through each row
             foreach (var row in rows)
@@ -72,11 +103,11 @@ namespace finance_scraper_1
                 else
 				{
 					// To print the data into the console and tab space between text
-					Console.WriteLine(rows[0].Text.Replace(" ", "\t\t"));
+					Console.WriteLine(rows[0].Text.Replace(" ", "\t"));
 				}
 
                 // Print the data to the console
-				Console.WriteLine(strRowData);
+				System.Console.WriteLine(strRowData);
 
                 // 
 				strRowData = String.Empty;
